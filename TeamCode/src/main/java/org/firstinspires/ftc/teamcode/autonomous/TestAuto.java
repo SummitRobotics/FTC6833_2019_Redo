cc2b5355b074fe005b43408fbfac0251ec551767
Chase <1chasemcd@gmail.com>
2019-01-23 16:17:03 -0800

    Update new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAuto.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-public class TestAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.autonomous.actions.CoreAction;
+import org.firstinspires.ftc.teamcode.autonomous.actions.LegControl;
+import org.firstinspires.ftc.teamcode.autonomous.actions.MarkerServo;
+import org.firstinspires.ftc.teamcode.autonomous.actions.MoveByEncoder;
+import org.firstinspires.ftc.teamcode.autonomous.actions.SampleDetection;
+
+import java.util.ArrayList;
+
+@Autonomous(name="TestAuto", group="LinearOpMode")
+public class TestAuto extends CoreAuto {
+
+    //Initializes action list
+    private ArrayList<CoreAction> path = new ArrayList<>();
+
+    @Override
+    public void runOpMode() {
+        // Add paths for autonomous
+        path.add(new LegControl(0.1, 0.1, 0.1, END));
+
+
+        // Update telemetry
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        telemetry.addData("Status", "Running");
+
+        // Run the paths created earlier
+        runPath(path);
+    }
 }
