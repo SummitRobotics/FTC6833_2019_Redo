61e81b73f558e5e07bb95fed32de8d83d4b34aeb
django <django@inventobot.com>
2019-03-06 15:59:15 -0800

    changea auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CraterSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CraterSideAuto.java
@@ -20,8 +20,9 @@ public class CraterSideAuto extends CoreAuto {
     @Override
     public void runOpMode() {
         path.add(new LegControl(0.55, -0.45, 1, 1,1));
-        path.add(new MoveByEncoder(6, 0.2, 1));
+        path.add(new MoveByEncoder(6, 0.25, 1));
         path.add(new LegControl(0, 0.17, 0, 1, 1));
+        //path.add(new MoveByEncoder(3, 0.25 , 1));
         path.add(new WaitForTime(0.3, 1));
         path.add(new SampleDetection(1, 9, 16));
 
@@ -30,23 +31,27 @@ public class CraterSideAuto extends CoreAuto {
         path.add(new MoveByEncoder(-13, 13, 0.2,  0.2, 1));
         path.add(new MoveByEncoder(-25, 0.2, 1));
         path.add(new MoveByEncoder(19, -19, 0.2,  0.2, 1));
-        path.add(new MoveByEncoder(75, 0.2, 1));
+        path.add(new MoveByEncoder(60, 0.2, 1));
 
-        path.add(new ArmControl(0.5, 0.5, 1));
-        path.add(new ArmControl(-0.5, 0.5, 1));
+        path.add(new ArmControl(0.25, 0.5, 1));
 
-        path.add(new MoveByEncoder(-75, 0.2, END));
+
+        path.add(new MoveByEncoder(-80, 0.2, END));
 
         // Center path
+        //path.add(new MoveByEncoder(-3, 0.2, 1));
         path.add(new LegControl(0, -0.17, 0, 1, 1));
-        path.add(new MoveByEncoder(-15, 0.2, 1));
-        path.add(new MoveByEncoder(-35, 35, 0.2,  0.2, 1));
-        path.add(new MoveByEncoder(25, 0.2, 1));
+        path.add(new MoveByEncoder(-13, 0.25, 1));
+        path.add(new MoveByEncoder(3,0.25,1));
+        path.add(new MoveByEncoder(23, -23, 0.2,  0.2, 1));
+        path.add(new MoveByEncoder(35, 0.25, 1));
+        path.add(new MoveByEncoder(-10, 10, 0.25, 1));
+        path.add(new MoveByEncoder(40,0.25,1));
 
-        path.add(new ArmControl(0.5, 0.5, 1));
+        path.add(new ArmControl(0.25, 0.5, 1));
 
         path.add(new MoveByEncoder(-13, 13, 0.2,  0.2, 1));
-        path.add(new MoveByEncoder(-75, 0.2, END));
+        path.add(new MoveByEncoder(-75, 0.25, END));
 
         // Right Path
         path.add(new LegControl(0, -0.17, 0, 1, 1));
